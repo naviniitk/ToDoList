fb80e1cc9dc8130c2bd3f04a10635017d588fefe
Naveen Kumar <navin10102001@gmail.com>
2021-06-10 23:50:27 +0530

    simple layout

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";const t=document.getElementById("dropbtn"),e=document.getElementById("sidebar");t.onclick={mydropDown:t=>{t.classList.toggle("show")}}(e)})();
+(()=>{"use strict";document.getElementById("btn").onclick=function(){document.getElementById("myDropdown").classList.toggle("show")},window.onclick=function(t){if(!t.target.matches(".dropbtn")){const t=document.getElementsByClassName("dropdown-content");let n;for(n=0;n<t.length;n++){const o=t[n];o.classList.contains("show")&&o.classList.remove("show")}}}})();
