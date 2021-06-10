3ddfd81f7dbc9f18a4107a577f1174438c8f235c
Naveen Kumar <navin10102001@gmail.com>
2021-06-11 04:40:05 +0530

    added a form and accessing it is easy now just showing the form input would complete for now

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import dropDowm from './dropdown';
+function Remind (title, desc, date) {
+  this.title = title;
+  this.desc = desc;
+  this.date = date;
+}
 
 function myFunction() {
   document.getElementById('myDropdown').classList.toggle('show');
@@ -10,8 +14,7 @@ button.onclick = myFunction;
 window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
     const dropdowns = document.getElementsByClassName('dropdown-content');
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
+    for (let i = 0; i < dropdowns.length; i++) {
       const openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
@@ -19,3 +22,30 @@ window.onclick = function (event) {
     }
   }
 };
+
+const add = document.getElementById('add');
+add.onclick = () => {
+  document.getElementById('fillform').style.display = 'block';
+};
+
+const close = document.getElementById('close');
+close.onclick = () => {
+  document.getElementById('fillform').style.display = 'none';
+};
+
+const events = document.getElementById('events');
+
+document.getElementById('submit').onclick = () => {
+  const title = document.getElementById('title').value;
+  const desc = document.getElementById('description').value;
+  const date = document.getElementById('date').value;
+  const reminder = new Remind(title, desc, date);
+  list.push(reminder);
+  showList(list);
+};
+
+const list = [];
+
+function showList(e) {
+  document.getElementById('events').innerText = e.title;
+}
